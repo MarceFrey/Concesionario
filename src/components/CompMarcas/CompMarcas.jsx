27e95fe7951ef46a1f8a1746cec158e5a4fe158b
MarceFrey <you@example.com>
2025-06-04 19:33:32 -0300

    Se cambian las imagenes a la carpeta public

--- src/components/CompMarcas/CompMarcas.jsx
+++ src/components/CompMarcas/CompMarcas.jsx
@@ -2,14 +2,14 @@ import './CompMarcas.css'
 import { Link } from 'react-router-dom'
 
 const logos = [
-  { id: 1, src: '/src/assets/volkswagen.png', alt: 'Volkswagen', path: '/marcas/volkswagen' },
-  { id: 2, src: '/src/assets/chevrolet.png', alt: 'Chevrolet', path: '/marcas/chevrolet' },
-  { id: 3, src: '/src/assets/toyota.png', alt: 'Toyota', path: '/marcas/toyota' },
-  { id: 4, src: '/src/assets/honda.png', alt: 'Honda', path: '/marcas/honda' },
-  { id: 5, src: '/src/assets/renault.png', alt: 'Renault', path: '/marcas/renault' },
-  { id: 6, src: '/src/assets/fiat.png', alt: 'Fiat', path: '/marcas/fiat' },
-  { id: 7, src: '/src/assets/ford.png', alt: 'Ford', path: '/marcas/ford' },
-  { id: 8, src: '/src/assets/peugeot.png', alt: 'Peugeot', path: '/marcas/peugeot' },
+  { id: 1, src: '/public/volkswagen.png', alt: 'Volkswagen', path: '/marcas/volkswagen' },
+  { id: 2, src: '/public/chevrolet.png', alt: 'Chevrolet', path: '/marcas/chevrolet' },
+  { id: 3, src: '/public/toyota.png', alt: 'Toyota', path: '/marcas/toyota' },
+  { id: 4, src: '/public/honda.png', alt: 'Honda', path: '/marcas/honda' },
+  { id: 5, src: '/public/renault.png', alt: 'Renault', path: '/marcas/renault' },
+  { id: 6, src: '/public/fiat.png', alt: 'Fiat', path: '/marcas/fiat' },
+  { id: 7, src: '/public/ford.png', alt: 'Ford', path: '/marcas/ford' },
+  { id: 8, src: '/public/peugeot.png', alt: 'Peugeot', path: '/marcas/peugeot' },
 ]
 
 const CompMarcas = () => {
