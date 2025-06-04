c83bc3ef7326ccbfb46716dc93a05f76a0bf5749
MarceFrey <you@example.com>
2025-06-04 20:00:51 -0300

    Se corrigen algunas rutas de imagenes

--- src/components/CompMarcas/CompMarcas.jsx
+++ src/components/CompMarcas/CompMarcas.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const logos = [
   { id: 1, src: '/volkswagen.png', alt: 'Volkswagen', path: '/marcas/volkswagen' },
   { id: 2, src: '/chevrolet.png', alt: 'Chevrolet', path: '/marcas/chevrolet' },
-  { id: 3, src: '/public/toyota.png', alt: 'Toyota', path: '/marcas/toyota' },
-  { id: 4, src: '/public/honda.png', alt: 'Honda', path: '/marcas/honda' },
-  { id: 5, src: '/public/renault.png', alt: 'Renault', path: '/marcas/renault' },
-  { id: 6, src: '/public/fiat.png', alt: 'Fiat', path: '/marcas/fiat' },
-  { id: 7, src: '/public/ford.png', alt: 'Ford', path: '/marcas/ford' },
-  { id: 8, src: '/public/peugeot.png', alt: 'Peugeot', path: '/marcas/peugeot' },
+  { id: 3, src: '/toyota.png', alt: 'Toyota', path: '/marcas/toyota' },
+  { id: 4, src: '/honda.png', alt: 'Honda', path: '/marcas/honda' },
+  { id: 5, src: '/renault.png', alt: 'Renault', path: '/marcas/renault' },
+  { id: 6, src: '/fiat.png', alt: 'Fiat', path: '/marcas/fiat' },
+  { id: 7, src: '/ford.png', alt: 'Ford', path: '/marcas/ford' },
+  { id: 8, src: '/peugeot.png', alt: 'Peugeot', path: '/marcas/peugeot' },
 ]
 
 const CompMarcas = () => {
